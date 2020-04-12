afb1bf83f08a53960687cfc98d7539a416459d2c
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2020-04-12 23:35:28 +0200

    remove all examples

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
 
-import { useFormatMoney } from "react-use-pkg";
-
 const App = () => {
-  const [value, setFormat] = useFormatMoney();
-  const input = React.useRef(null);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    setFormat(input.current.value, "de", "EUR");
-  };
-
   return (
     <div>
-      <h1>useFormatMoney hook</h1>
-      <form onSubmit={onSubmit}>
-        <input ref={input} type="text" name="amount" />
-        <button type="submit">Set Format Money</button>
-      </form>
-      <h3>{value}</h3>
+      <h1>useFetchWithCORS</h1>
     </div>
   );
 };
