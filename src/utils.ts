a04f7b63323983d5e7f470d58f1fa9d8e38ab7b7
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2020-04-12 23:24:13 +0200

    refactor fetcher

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,2 @@
-export const fetcher = (url: string) => fetch(url).then((res) => res.json());
+export const fetcher = (url: RequestInfo, opt?: RequestInit) =>
+  fetch(url, opt).then((res) => res.json());
